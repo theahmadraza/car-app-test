8fb2f519167ded45c6883a605c6a5997aff8955e
ahmad <csmirza11@gmail.com>
2024-03-19 11:27:00 +0500

    new

--- server/middlewares/IsAuthorized.js
+++ server/middlewares/IsAuthorized.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken')
 const IsAuthorized = (req, res,  next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        console.log(token, "=============================")
         const verify = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
         req.decodeToken = verify;
         next();
